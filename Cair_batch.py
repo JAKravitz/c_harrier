b9be2b428555bcb44708c1d480edce4df42649d9
jakravit <jakravit@forza2.ndc.nasa.gov>
2022-03-14 14:30:32 -0500

    datetime updates

--- Cair_batch.py
+++ Cair_batch.py
@@ -14,9 +14,9 @@ import matplotlib.dates as mdates
 import re
 
 sensor = 'lt'
-diry = 'today20211027'
+diry = 'today20211028'
 path = '/Users/jakravit/OneDrive - NASA/C-HARRIER_2021_C-AIR/C-AIR/'
-outpath = '/Users/jakravit/Desktop/Cair_figs/'
+outpath = '/Users/jakravit/Desktop/'
 
 diry = diry+'/'
 date = re.findall('[0-9]+', diry)[0]
@@ -34,8 +34,8 @@ for f in flist:
             name = f[:-4]
             file = path+diry+f
             data = pd.read_csv(file, sep=',', encoding='ISO-8859-1', index_col='DateTimeUTC')
-            data.index = pd.to_datetime(data.index)  
-            
+            data.index = pd.to_datetime(data.index)
+            # data.index = data.index.strftime("%m/%d/%y, %H:%M:%S")
             sensors = {'es': data.iloc[:,5:24],
                         'li': data.iloc[:,24:43],
                         'lt': data.iloc[:,43:]}
@@ -53,10 +53,31 @@ plt.xticks(rotation=45,fontsize=14)
 ax.set_ylabel('uW/cm2/nm',fontsize=14)
 ax.set_title('{} - {} -- {}'.format(date,name,sensor),fontsize=16)
 ax.set_ylim(0,20)
-fig.savefig('{}/{}_{}.png'.format(outpath+diry,name,sensor),bbox_inches='tight',dpi=300)
+# fig.savefig('{}/{}_{}.png'.format(outpath+diry,name,sensor),bbox_inches='tight',dpi=300)
 
 
+#%%
+fig, ax = plt.subplots(figsize=(10,6))
+sensors = {'es': data.iloc[:,5:24],
+            'li': data.iloc[:,24:43],
+            'lt': data.iloc[:,43:]}
 
-            
-            
+   
+colormap = plt.cm.gist_ncar
+plt.gca().set_prop_cycle(plt.cycler('color', plt.cm.jet(np.linspace(0, 1, sensors[sensor].shape[1]))))
+labels = []
+for i,b in enumerate(sensors[sensor].columns):
+    ax.plot(data.index,data[b],) #marker='o',s=2)
+    labels.append('{}'.format(b.split(' ')[0]))    
+plt.xticks(rotation=45,fontsize=14)   
+
+hour = mdates.HourLocator()   # every hour
+mins = mdates.MinuteLocator(byminute=[0,30],interval=1)  # every minute
+hourFmt = mdates.DateFormatter('%H:%M')
+ax.xaxis.set_major_locator(hour)
+ax.xaxis.set_major_formatter(hourFmt)
+ax.xaxis.set_minor_locator(mins)
+
+myFmt = mdates.DateFormatter("%H:%M")
+ax.xaxis.set_major_formatter(myFmt)            
             
